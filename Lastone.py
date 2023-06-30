3fcd79f5257ab36698ec2303f2d06ed4c883e1b3
Tonatiuh Hernández <66359772+Batzem@users.noreply.github.com>
2023-06-30 15:39:12 -0600

    Add files via upload

---
+++ Lastone.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  9 17:46:15 2023
+
+@author: ESFM
+"""
+
+import numpy as np
+#Modelo de Bolizmann
+#---------------------------
+#---------------------------
+#   Vector en R3
+#---------------------------
+outputs = softmax(x)
+print("sofmax numpy: ", outputs)
