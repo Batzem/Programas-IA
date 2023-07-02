4290f95eaa44bc8dabab6ca3126c17d09c05aa80
Tonatiuh Hernández <66359772+Batzem@users.noreply.github.com>
2023-07-01 23:31:33 -0600

    Update laberinto.py

--- Laberinto_comentado/laberinto.py
+++ Laberinto_comentado/laberinto.py
@@ -1,1 +1,19 @@
+from pyamaze import maze,agent
 
+#-----------------------------------------
+#    Crear laberinto
+#    x,y posición de la meta
+#-----------------------------------------
+m= maze(25,40)
+m.CreateMaze(x=1, y=1)
+
+#----------------------------------
+# Poner agente en el laberinto
+#-----------------------------------
+a = agent(m,footprints=True,filled=True)
+
+#---------------------------------------
+#  Graficar la trayectoria del agente
+#---------------------------------------
+m.tracePath({a:m.path},delay=25
+m.run()
